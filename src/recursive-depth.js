f6072a5f71eb9ef9799c713b50bab795fa72f03c
MikolaSh <nikolashulga2000@gmail.com>
2021-09-15 11:55:33 +0300

    feat: recusive-depth half solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,8 +13,14 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth( arr ) {
+    let output = 1;
+   for(let i =0;i<arr.length; i++){
+     if(Array.isArray(arr[i])){
+      output+=calculateDepth(arr[i]);
+     }
+   }
+   console.log(output)
+   return output;
   }
 }
