1507bdd9de496e92111e1b33f9f24313a3d394de
MikolaSh <nikolashulga2000@gmail.com>
2021-09-15 09:19:58 +0300

    feat: st-common-character-count solution

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount( s1, s2 ) {
+  if(s1==''||s2==''){return 0}
+  console.log(s1);
+  console.log(s2)
+  let counter =0;
+  for(let i=0;i<s1.length;i++){
+    for(let j=0;j<s2.length;j++){
+      if(s1[i]===s2[j]){
+        counter++;
+        s2 = s2.split("");
+        console.log(s2)
+        s2[j]='*';
+        s2 = s2.join("");
+        break;
+      }
+    }
+  }
+  return counter
 }
